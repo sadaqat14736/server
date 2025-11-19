313e72fe134dfdd8159b006a3bacd5e0bb3ba8bf
sadaqat14736 <sadaqat.mec786@gmail.com>
2025-11-19 01:33:55 -0800

    updated

--- controller/auth.js
+++ controller/auth.js
@@ -39,17 +39,51 @@ async function signUp(req, res) {
     }
 }
 
-async function login() {
-    try{
 
-        const { email, password } = req.body;
+async function login(req, res,) {
+  // destructure
+  try {
+    const { email, password } = req.body;
 
-        if(!email || !password){
-            return res.send({
-                status: 400,
-            })
-        }
-    }
+    const dbUser = await userModel.findOne({ email });
+    console.log(dbUser, "here is a user");
+
+    // Load hash from your password DB.
+    bcrypt.compare(password, dbUser.password, function (err, result) {
+      // result == true
+
+      if (result) {
+        console.log(process.env.JWTSECRETKEY, "process.env.JWTSECRETKEY");
+        let token = jwt.sign(
+          {
+            email: dbUser.email,
+            firstName: dbUser.firstName,
+            "last name": dbUser.lastName,
+            role: dbUser.role,
+          },
+          process.env.JWTSECRETKEY,
+          { expiresIn: "1d" }
+        );
+        console.log(token);
+        res.cookie("jwtToken", token, {
+          httpOnly: true,
+          maxAge: "1d", // 1 day in milliseconds
+        });
+        res.send({
+          status: 200,
+          message: "user login successfully",
+          token,
+        });
+      }
+    });
+  } catch (err) {
+    res.send({
+      err,
+      status: 500,
+      message: "sorry! server is not responding",
+    });
+  }
 }
 
+ 
 module.exports = { signUp, login };
--- router/route.js
+++ router/route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { signUp, login } = require("../controller/auth");
+const authrization = require('../middleware/authentication');
 
 
 const router = express.Router();
 
 router.post("/signup", signUp);
-router.post("/login", login);
+router.post("/login",authrization, login);
+
 
 router.get("/test", (req, res) => {
     res.send("this is a test route");
